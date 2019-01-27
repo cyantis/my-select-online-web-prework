52c1e0714a639f5d53556cb5101108b84c9dbce0
Cody Yantis <cyantis@gmail.com>
2019-01-27 17:09:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(array)
   i = 0
   new_arr = []
   while i < array.length
-    if yield(array[i]) != nil
-      new_arr << yield(array[i])
+    if yield(array[i]) == true
+      new_arr << array[i]
     end
     i += 1
   end
